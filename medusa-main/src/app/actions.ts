afd74c695b1038018d704460dacd302695ba1f20
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 19:56:30 -0800

    Update actions.ts

--- medusa-main/src/app/actions.ts
+++ medusa-main/src/app/actions.ts
@@ -2,11 +2,8 @@
 
 import { revalidateTag } from "next/cache"
 
-/**
- * Revalidates each cache tag in the passed array
- * @param {string[]} tags - array of tags to revalidate
- */
-export async function revalidateTags(tags: string[]) {
+export async function revalidateTags(tags: string[])
+{
   tags.forEach((tag) => {
     revalidateTag(tag)
   })
