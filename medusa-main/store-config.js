7932d47e81d5690a060a7e5a87479db1cfc65d31
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 19:50:34 -0800

    Update store-config.js

--- medusa-main/store-config.js
+++ medusa-main/store-config.js
@@ -1,11 +1,12 @@
-function withStoreConfig(nextConfig = {}) {
+function withStoreConfig(nextConfig = {}) 
+{
   const features = nextConfig.features || {}
   delete nextConfig.features
 
   nextConfig.env = nextConfig.env || {}
-
   Object.entries(features).forEach(([key, value]) => {
-    if (value) {
+    if (value) 
+    {
       nextConfig.env[`FEATURE_${key.toUpperCase()}_ENABLED`] = true
     }
   })
