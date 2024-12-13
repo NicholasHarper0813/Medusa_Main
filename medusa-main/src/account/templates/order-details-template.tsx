1b75cd1f0f79798de70ef2b02e7ae8d17ec0a6fe
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 19:55:45 -0800

    Update order-details-template.tsx

--- medusa-main/src/account/templates/order-details-template.tsx
+++ medusa-main/src/account/templates/order-details-template.tsx
@@ -4,17 +4,16 @@ import { useRouter } from "next/router"
 
 const OrderDetailsTemplate = () => {
   const router = useRouter()
-
   const { order } = router.query
-
   const { order: details, isLoading } = useOrder(order as string, {
     enabled: !!order,
   })
 
-  if (isLoading || !details) {
+  if (isLoading || !details) 
+  {
     return <div>Loading...</div>
   }
-
+  
   return <OrderCompletedTemplate order={details} />
 }
 
