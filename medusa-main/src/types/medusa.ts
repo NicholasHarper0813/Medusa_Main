565f04b2e614a6b9540b7bab8cf02c1ea20a0f5c
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 17:48:18 -0800

    Update medusa.ts

--- medusa-main/src/types/medusa.ts
+++ medusa-main/src/types/medusa.ts
@@ -5,13 +5,10 @@ import {
 } from "@medusajs/medusa"
 
 export type Variant = Omit<ProductVariant, "beforeInsert">
-
 export interface Product extends Omit<MedusaProduct, "variants"> {
   variants: Variant[]
 }
-
 export interface Region extends Omit<MedusaRegion, "beforeInsert"> {}
-
 export type CalculatedVariant = ProductVariant & {
   calculated_price: number
   calculated_price_type: "sale" | "default"
