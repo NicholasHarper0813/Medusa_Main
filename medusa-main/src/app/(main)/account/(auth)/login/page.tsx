c11326dfab698a4aadc1241a0753089f2e0f2fca
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 17:27:09 -0800

    Update page.tsx

--- medusa-main/src/app/(main)/account/(auth)/login/page.tsx
+++ medusa-main/src/app/(main)/account/(auth)/login/page.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   description: "Sign in to your ACME account.",
 }
 
-export default function Login() {
+export default function Login() 
+{
   return <LoginTemplate />
 }
