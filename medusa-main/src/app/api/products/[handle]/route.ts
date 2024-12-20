98077e81d7a5005839628abdaffe31dbcb74b674
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 17:37:35 -0800

    Update route.ts

--- medusa-main/src/app/api/products/[handle]/route.ts
+++ medusa-main/src/app/api/products/[handle]/route.ts
@@ -1,22 +1,13 @@
 import { NextResponse, NextRequest } from "next/server"
 import { initialize as initializeProductModule } from "@medusajs/product"
 
-/**
- * This endpoint uses the serverless Product Module to retrieve a product by handle.
- * The module connects directly to your Medusa database to retrieve and manipulate data, without the need for a dedicated server.
- * Read more about the Product Module here: https://docs.medusajs.com/modules/products/serverless-module
- */
 export async function GET(
   request: NextRequest,
   { params }: { params: Record<string, any> }
-) {
-  // Extract the query parameters
+) 
+{
   const { handle } = params
-
-  // Initialize the Product Module
   const productService = await initializeProductModule()
-
-  // Run the query
   const products = await productService.list(
     { handle },
     {
@@ -35,6 +26,5 @@ export async function GET(
     }
   )
 
-  // Return the response
   return NextResponse.json({ products })
 }
