55c19d4076a8e37613f9d421e82b86d2a063ccb8
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 17:44:05 -0800

    Update index.tsx

--- medusa-main/src/modules/skeletons/components/skeleton-order-confirmed-header/index.tsx
+++ medusa-main/src/modules/skeletons/components/skeleton-order-confirmed-header/index.tsx
@@ -4,8 +4,8 @@ const SkeletonOrderConfirmedHeader = () => {
       <div className="w-2/5 h-4 bg-gray-100"></div>
       <div className="w-3/6 h-6 bg-gray-100"></div>
       <div className="flex gap-x-4">
-        <div className="w-16 h-4 bg-gray-100"></div>
-        <div className="w-12 h-4 bg-gray-100"></div>
+      <div className="w-16 h-4 bg-gray-100"></div>
+      <div className="w-12 h-4 bg-gray-100"></div>
       </div>
     </div>
   )
